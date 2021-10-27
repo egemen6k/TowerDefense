5760d0046f3b371f10a13a6fd834e45494add652
egemen6k <egemen.altikardesler@gmail.com>
2021-10-27 22:48:33 +0300

    +EnemySpawn,Countdown,Wavespawn

---
+++ TowerDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+
+    public Text waveCountDownText;
+
+    private int waveIndex = 1;
+
+
+    private void Update()
+    {
+        if (countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+
+        waveCountDownText.text = Mathf.Round(countDown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
