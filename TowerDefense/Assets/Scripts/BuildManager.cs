16b47ecd3225f90a21421c2b7a5a52fd18d8c744
egemen6k <egemen.altikardesler@gmail.com>
2021-11-01 22:52:52 +0300

    +buildmanager system

---
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
--- TowerDefense/Assets/Scripts/Node.cs
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -5,12 +5,28 @@ public class Node : MonoBehaviour
     public Color hoverColor;
     private Color startColor;
 
+    public Vector3 offset;
+
     private Renderer rend;
+
+    private GameObject turret;
 
     private void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cant build");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.Instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + offset , transform.rotation);
     }
 
     private void OnMouseEnter()
