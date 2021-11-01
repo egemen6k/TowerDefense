738e908be2e09f73168949ff4c05a0119594a340
egemen6k <egemen.altikardesler@gmail.com>
2021-11-01 22:35:51 +0300

    +Onmouseenter color set

---
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
