4e5ee78d8a3164caf032ebdb99d1298d546ad203
egemen6k <egemen.altikardesler@gmail.com>
2021-11-01 20:39:00 +0300

    +Fire countdown & header organization

--- TowerDefense/Assets/Scripts/Turret.cs
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
-    public float range = 15f;
 
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
+
+
 
     void Start()
     {
@@ -54,6 +61,19 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, turnSpeed * Time.deltaTime).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1 / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
     }
 
     private void OnDrawGizmosSelected()
