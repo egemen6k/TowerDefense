c433f73c11a81beab802d1088a5ff115712712e6
egemen6k <egemen.altikardesler@gmail.com>
2021-11-02 21:56:04 +0300

    +Missile launcher selection&placement

--- TowerDefense/Assets/Scripts/Shop.cs
+++ TowerDefense/Assets/Scripts/Shop.cs
@@ -14,9 +14,9 @@ public class Shop : MonoBehaviour
         buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
     }
 
-    public void PurchaseAnotherTurret()
+    public void PurchaseMissileLauncher()
     {
-        Debug.Log("Another turret selected.");
-        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+        Debug.Log("Missile launcher selected.");
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
     }
 }
