0c9ce24ec30fd34a11e978ad19a89cf374d1a02d
egemen6k <egemen.altikardesler@gmail.com>
2021-11-05 21:45:04 +0300

    +UI changes

--- TowerDefense/Assets/Scripts/BuildManager.cs
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -20,6 +20,8 @@ public class BuildManager : MonoBehaviour
     public GameObject standardTurretPrefab;
     public GameObject missileLauncherPrefab;
 
+    public GameObject buildEffect;
+
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
@@ -42,6 +44,9 @@ public class BuildManager : MonoBehaviour
 
         GameObject turret = Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect = Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Turret build ! Money Left: " + PlayerStats.Money);
     }
