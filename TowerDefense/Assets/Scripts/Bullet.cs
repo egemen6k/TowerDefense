1e318259c2fc8f6bc342044082f7dd3bbc46831f
egemen6k <egemen.altikardesler@gmail.com>
2021-11-05 22:34:17 +0300

    +Damage system & money

--- TowerDefense/Assets/Scripts/Bullet.cs
+++ TowerDefense/Assets/Scripts/Bullet.cs
@@ -9,6 +9,7 @@ public class Bullet : MonoBehaviour
     public float explosionRadius = 0f;
     public GameObject impactEffect;
 
+    public int damage = 50;
     public void Seek(Transform _target)
     {
         target = _target;
@@ -68,7 +69,12 @@ public class Bullet : MonoBehaviour
 
     void Damage(Transform enemy)
     {
-        Destroy(enemy.gameObject);
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
     }
 
     private void OnDrawGizmosSelected()
--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -5,12 +5,32 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
 
+    public int health = 100;
+
+    public int value = 50;
+
     private Transform target;
     private int wavepointIndex = 0;
 
     private void Start()
     {
         target = Waypoints.points[0];
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        PlayerStats.Money += value;
+        Destroy(gameObject);
     }
 
     private void Update()
