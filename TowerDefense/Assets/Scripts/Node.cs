2254efd0574c452782dee347218e2b0b18258bf8
egemen6k <egemen.altikardesler@gmail.com>
2021-11-11 14:24:24 +0300

    +Upgrade&sell ui

--- TowerDefense/Assets/Scripts/Node.cs
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -29,14 +29,14 @@ public class Node : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject())
             return;
 
-        if (!buildManager.CanBuild)
-            return;
-
         if (turret != null)
         {
-            Debug.Log("Cant build");
+            buildManager.SelectNode(this);
             return;
         }
+
+        if (!buildManager.CanBuild)
+            return;
 
         buildManager.BuildTurretOn(this);
     }
