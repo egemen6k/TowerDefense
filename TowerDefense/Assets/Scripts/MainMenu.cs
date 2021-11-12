d9b0a04e5fd110b308c6e0bb99abf531b0e33966
egemen6k <egemen.altikardesler@gmail.com>
2021-11-12 15:38:39 +0300

    +Main menu

---
+++ TowerDefense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
